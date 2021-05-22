94471620714537ccf8f5a7f4541e7ad741b62c8f
yoshimitsumatsutaIe <sbb03104@edu.osakafu-u.ac.jp>
2021-05-22 17:04:48 +0900

    gakko

--- exercise/exercise_4.py
+++ exercise/exercise_4.py
@@ -7,3 +7,33 @@
 
 2. 比例ゲイン，微分ゲイン，積分ゲインを変化させたとき，変位xの時間変化がどう変化するかアニメーションで示してください．
 """
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as anm
+
+
+class PID:
+    """"""
+    
+    def __init__(self, M = 1.0, K = 1.0, D = 1.0, INIT = 0.0, GOAL = 1.0):
+        self.M = M  # 質量
+        self.K = K  # ばね乗数
+        self.D = D  # 摩擦係数
+        self.INIT = INIT  # 初期値
+        self.GOAL = GOAL  # 目標値
+    
+    
+    def sim(self, Kp, Ki, Kd):
+        """テスト"""
+        
+        
+        
+        u = 
+
+
+
+
+if __name__ == '__main__':
+    hoge = PID()
+    
