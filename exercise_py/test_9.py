feb94ce0a6882218f4699ab23e2e8194c5d231c2
yoshimitsumatsutaIe <sbb03104@edu.osakafu-u.ac.jp>
2021-06-10 01:03:00 +0900

    9

--- exercise_py/test_9.py
+++ exercise_py/test_9.py
@@ -2,7 +2,7 @@ import math
 from os import close
 import numpy as np
 import matplotlib.pyplot as plt
-
+import time
 
 def map_example():
     map = [
@@ -25,8 +25,8 @@ def option(gridmap, id):
     # x_max = map.shape[1]
     # y_max = map.shape[0]
     
-    x_max = len(gridmap[0])
-    y_max = len(gridmap)
+    x_max = len(gridmap[0])-1
+    y_max = len(gridmap)-1
     
     options = []
     
@@ -55,15 +55,27 @@ def option(gridmap, id):
         if not gridmap[y][x+1]:
             options.append([1, 0, 1])
     
-    # print('minimap')
-    # [print(row) for row in [gridmap[y-1][x-1:x+2], gridmap[y][x-1:x+2],gridmap[y+1][x-1:x+2],]]
-    # print('options')
-    # [print(option) for option in options]
+    print('center')
+    print(x, y)
+    #print('minimap')
+    #[print(row) for row in [gridmap[y-1][x-1:x+2], gridmap[y][x-1:x+2],gridmap[y+1][x-1:x+2],]]
+    print('options')
+    [print(option) for option in options]
     
     return options
 
-option(map_example(), (5, 1))
+#option(map_example(), (5, 1))
 
+
+def calc_path(goal_node, closet_set):
+    rx, ry = [goal_node.x], [goal_node.y]
+    parent = goal_node.parent
+    while parent != (-1, -1):
+        node = closed_set[parent]
+        rx.append(node.x)
+        ry.append(node.y)
+        parent = node.parent
+    return rx, ry
 
 class Node:
     def __init__(self, x, y, cost, parent):
@@ -76,7 +88,7 @@ open_set = dict()
 closed_set = dict()
 
 
-start_node = Node(0, 0, 0, -1)
+start_node = Node(0, 0, 0, (-1, -1))
 goal_node = Node(9, 9, float('inf'), None)
 
 
@@ -87,22 +99,72 @@ start_id = (start_node.x, start_node.y)
 open_set[start_id] = start_node
 
 
-# for i in range(1):
-    
-#     temp_id = min(open_set, key=lambda o: open_set[o].cost)
-#     temp = open_set[temp_id]
-    
-#     if temp.x == goal_node.x & temp.y == goal_node.y:
-#         print('終わり')
-#         goal_node.parent = temp.parent
-#         goal_node.cost = temp.cost
-#         break
+while True:
     
     
-#     del open_set[temp_id]
-#     closed_set[temp_id] = temp
+    temp_id = min(open_set, key=lambda o: open_set[o].cost)
+    print('temp_id = ', temp_id)
+    temp = open_set[temp_id]
+    # time.sleep(0.5)
+    # plt.plot(temp.x, temp.y, "xc")
+    # plt.gcf().canvas.mpl_connect(
+    #     'key_release_event',
+    #     lambda event: [exit(0) if event.key == 'escape' else None])
+    # if len(closed_set.keys()) % 10 == 0:
+    #     plt.pause(0.001)
     
     
-#     for x, y, cost in option(map_example(), temp_id):
+    
+    if temp.x == goal_node.x & temp.y == goal_node.y:
+        print('終わり')
+        goal_node.parent = temp.parent
+        goal_node.cost = temp.cost
+        break
+    
+    
+    del open_set[temp_id]
+    closed_set[temp_id] = temp
+    
+    
+    for dx, dy, dcost in option(map_example(), temp_id):
+        node = Node(temp.x + dx, temp.y + dy, temp.cost + dcost, temp_id)
+        node_id = (node.x, node.y)
         
+        if node_id in closed_set:
+            """決定済みのとき"""
+            continue
         
+        if node_id not in open_set:
+            """未探索のとき"""
+            open_set[node_id] = node
+        else:
+            """未決定のとき"""
+            if open_set[node_id].cost >= node.cost:
+                open_set[node_id] = node
+
+
+print('OK!')
+
+
+
+rx, ry = calc_path(goal_node, closed_set)
+
+print(rx)
+print(ry)
+#ry = np.array(ry)
+#ry = 10 - ry
+
+map_ = np.array(map_example())
+obs = np.where(map_ == 1)
+#obsy = 9 - obs[1]
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(rx, ry, marker = '.', color = 'r')
+ax.scatter(obs[1], obs[0], color = 'k', marker = '+')
+
+ax.grid(True)
+ax.axis('equal')
+ax.set_xlim(0, 9)
+ax.set_ylim(0, 9)
+plt.show()
