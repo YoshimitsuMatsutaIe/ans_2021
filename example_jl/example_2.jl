85608f13d182fff186cf2fa5b96a956322ea0ae6
yoshimitsumatsutaIe <matsuta2718@gmail.com>
2021-08-10 01:16:03 +0900

    ie

--- example_jl/example_2.jl
+++ example_jl/example_2.jl
@@ -1,25 +1,20 @@
 using DifferentialEquations
-using Plots;gr()
+using Plots
 
-#微分方程式を定義
-f(y , t) = 3.0*y + 2
 
-#初期値を設定
-y0 = 1.0
+f(y,t) = 3.0*y  #微分方程式を定義
+y0 = 1.0  # 初期値を設定
+tspan = (0.0 , 1.0)  # 解く範囲を設定
 
-#時間間隔を設定
-tspan = (0.0 , 1.0)
-
-#ODEProblemで解く
 prob = ODEProblem(f , y0 , tspan)
-sol = solve(prob)
+sol = solve(prob)  # ソルバで解く
 
-#plotする
-plot(sol,linewidth=5,title="Solution to the linear ODE with a thick line",
-     xaxis="Time (t)",yaxis="y(t) (in micro.m)",label="My Thick Line!")
-
-#厳密解を用意する
-g(t) = 5//3 * exp.(3*t) - 2//3
-
-#加えてplotする
-plot!(g , lw=3 , ls=:dash , label="True Solution!")
+# plot
+plot(
+     sol,
+     linewidth=5,
+     title="Solution",
+     xaxis="x",
+     yaxis="y(x)",
+     label="solution"
+)
