af2e1855577796deb137bddfce087d0dbba2de76
yoshimitsumatsutaIe <sbb03104@edu.osakafu-u.ac.jp>
2021-08-10 13:28:05 +0900

    gakko

--- example_jl/example_2.jl
+++ example_jl/example_2.jl
@@ -2,11 +2,11 @@ using DifferentialEquations
 using Plots
 
 
-a = 3.0
+a = 3.0  # パラメータ
 
-f(y,t,a) = a*y  #微分方程式を定義
+f(y, t, a) = a*y  #微分方程式を定義
 y0 = 1.0  # 初期値を設定
-tspan = (0.0 , 1.0)  # 解く範囲を設定
+tspan = (0.0, 1.0)  # 解く範囲を設定
 
 prob = ODEProblem(f , y0 , tspan)
 sol = solve(prob)  # ソルバで解く
@@ -15,8 +15,9 @@ sol = solve(prob)  # ソルバで解く
 plot(
      sol,
      linewidth=5,
-     title="Solution",
      xaxis="x",
      yaxis="y(x)",
      label="solution"
 )
+
+# savefig("solution_of_example_2.png")  #  保存
