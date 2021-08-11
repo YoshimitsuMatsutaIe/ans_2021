ead027e7a212b3aa34eb8d7b8d21e9988584306f
yoshimitsumatsutaIe <sbb03104@edu.osakafu-u.ac.jp>
2021-08-11 19:41:32 +0900

    gakkou

--- example_jl/example_3.jl
+++ example_jl/example_3.jl
@@ -1,53 +1,17 @@
-# using Plots
-# using CPUTime
-
-# @userplot CirclePlot
-# @recipe function f(cp::CirclePlot)
-#     x, y, i = cp.args
-#     n = length(x)
-#     inds = circshift(1:n, 1 - i)
-#     linewidth --> range(0, 10, length = n)
-#     seriesalpha --> range(0, 1, length = n)
-#     aspect_ratio --> 1
-#     label --> false
-#     x[inds], y[inds]
-# end
-
-# n = 150
-# t = range(0, 2π, length = n)
-# x = sin.(t)
-# y = cos.(t)
-
-# anim = @animate for i ∈ 1:n
-#     circleplot(x, y, i)
-# end
-# gif(anim, "anim_fps15.gif", fps = 15)
-
-
-
 using Plots
-default(legend = false)
-x = y = range(-5, 5, length = 40)
-zs = zeros(0, 40)
-n = 100
 
-@gif for i in range(0, stop = 2π, length = n)
-    f(x, y) = sin(x + 10sin(i)) + cos(y)
+anim = @animate for i in 1:200
+    #xs = range(0:0.1:2*pi)
+    scatter(
+        t->sinpi(t+i/100),
+        t->cospi(t+i/100),
+        [0],
+        size=(400,400),
+        xlims = (-1.5, 1.5),
+        ylims = (-1.5, 1.5),
+        aspect_ratio =1,
+    )
 
-    # create a plot with 3 subplots and a custom layout
-    l = @layout [a{0.7w} b; c{0.2h}]
-    p = plot(x, y, f, st = [:surface, :contourf], layout = l)
-
-    # induce a slight oscillating camera angle sweep, in degrees (azimuth, altitude)
-    plot!(p[1], camera = (10 * (1 + cos(i)), 40))
-
-    # add a tracking line
-    fixed_x = zeros(40)
-    z = map(f, fixed_x, y)
-    plot!(p[1], fixed_x, y, z, line = (:black, 5, 0.2))
-    vline!(p[2], [0], line = (:black, 5))
-
-    # add to and show the tracked values over time
-    global zs = vcat(zs, z')
-    plot!(p[3], zs, alpha = 0.2, palette = cgrad(:blues).colors)
 end
+gif(anim, "anim_fps15.gif", fps = 6)
+
