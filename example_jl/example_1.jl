e07c7c2bd5f7ceaa2482a94df49387e5c5632461
yoshimitsumatsutaIe <matsuta2718@gmail.com>
2021-08-12 19:13:19 +0900

    ie

--- example_jl/example_1.jl
+++ example_jl/example_1.jl
@@ -1,1 +1,25 @@
 using CPUTime
+using CSV
+using DataFrames
+
+path = "./example_jl/example_1_data"  # csvフォルダのパス
+
+
+cd(path)  # pathに移動
+file_name_all = readdir()  # カレントディレクトリにある全ファイル名取得
+csv_name = file_name_all[
+    map(x->occursin(r"\.csv$",x),file_name_all)
+]  # csvの名前だけ取得
+println(csv_name)
+
+for name in csv_name
+    println(name)
+    df =CSV.read(name, DataFrame)
+    println(df[df.x .> 40, :])
+    # if size(df[df.x .> 40, :]):> 0
+    #     println("yes")
+    # else
+    #     println("no")
+    # end
+end
+
