1b292bf6a8e40e437720ac30eb3247110d142bbd
yoshimitsumatsutaIe <matsuta2718@gmail.com>
2021-08-15 23:36:44 +0900

    ie

--- exercise_jl/exercise_5.jl
+++ exercise_jl/exercise_5.jl
@@ -2,23 +2,37 @@ using CPUTime
 using Plots
 using DifferentialEquations
 using LinearAlgebra
+using MatrixEquations
 
-
-function LQR(A, B, Q, R)
-    """有本ポッターで解く"""
+function ARE(A, B, Q, R)
+    """有本ポッターで解く（未完成）"""
     ℋ = [
         A' -B*inv(R)*B'
         -Q -A
     ]  #  ハミルトン行列
+    println("ℋ = ", ℋ)
 
     λ_ = eigvals(ℋ)  # ハミルトン行列の固有値
     ω_ = eigvecs(ℋ)  # ハミルトン行列の固有ベクトル
+    println("ℋ の固有値 = ", λ_)
+    println("ℋ の固有ベクトル", ω_)
 
-    index = findall(λ_.<0)  # ハミルトン行列の固有値が正のものを探す
+    # ハミルトン行列の固有値が負のものを探す
+    index = []
+    for i in 1:size(λ_)[1]
+        print(i)
+        if real(λ_[i]) < 0
+            println("実部が負")
+            push!(index, i)
+        else
+            println("実部が正")
+        end
+    end
+    println("ℋ の固有値の実部が負なのは, ", index)
     n = size(index)[1]
 
     # Y,Zを計算
-    ω = Matrix{Float64}(undef, size(ω_)[1], n)  # 未初期化のMatrix
+    ω = Matrix{ComplexF64}(undef, size(ω_)[1], n)  # 未初期化のMatrix
 
     for i in 1:n
         global ω[:, i] .= ω_[:, index[i]]
@@ -32,47 +46,89 @@ function LQR(A, B, Q, R)
     return P
 end
 
+function AREbyMatrixEquations(A, B, Q, R)
+    """ライブラリ使用
+    https://juliapackages.com/p/matrixequations
+    """
+    S = zero(B)
+    P, E, F = arec(A, B, R, Q, S)
+    return P
+    println("P = ", P)
+end
+
 
 function simu(A, B, C, D, Q, R, x0)
     """解を描写"""
-    P = LQR(A, B, Q, R)  # リカッチ方程式を解く
+    #P = ARE(A, B, Q, R)  # リカッチ方程式を解く
+    P = AREbyMatrixEquations(A, B, Q, R)
     K = inv(R) * B' * P  # 最適フィードバックゲイン
     Ā = A - B * K
     println("Ā = ", Ā)
     println(eigvals(Ā))
-    println("OK")
+    
     # 解
-    x(t) = exp(Ā .* t) * x0
-    # u(t) = -K * x(t)
-    # y(t) = C * x(t) + D * u(t)
-    #println(x(10))
+    x(t) = exp(Ā .* t) * x0  # 状態方程式の解
+    # u(t) = -K * x(t)  # 最適入力
+    # y(t) = C * x(t) + D * u(t)  # 出力方程式の解
+
     # Plot
-    x1(t) = x(t)[1]
-    plot(x1, 0, 10)
+    # t = 0:0.01:10
+    # sol = x.(t)
+    # println(typeof(sol))
+    # plot(sol)
+
+    
+    for i in 1:size(A)[1]
+        println(i)
+        if i == 1
+            plot(t -> x(t)[1], 0, 10)
+        else
+            plot!(t -> x[t][i], 0, 10)
+        end
+    end
+
+
+    # x1(t) = x(t)[1]
+    # x2(t) = x(t)[2]
+    # #x3(t) = x(t)[3]
+    # plot(x1, 0, 10)
+    # plot!(x2, 0, 10)
+    # #plot!(x3, 0, 10)
 end
 
 
 
 
 
-A = [
-    1.1 2.0 3.0
-    0 0.95 1.20
-    1.2 0.01 10.5
-]
-B = [
-    1.0
-    0.0
-    0.847
-]
-C = Matrix(I, 2, 2)
+# A = [
+#     1.1 2.0 3.0
+#     0 0.95 1.20
+#     1.2 0.01 10.5
+# ]
+# B = [
+#     1.0
+#     0.0
+#     0.847
+# ]
+# C = Matrix(I, 2, 2)
 
-# 重み行列
-Q = diagm(0 => [1,1,1]) .* 1000
-R = ones((1,1))
+# # 重み行列
+# Q = diagm(0 => [1,1,1]) .* 1000
+# R = ones((1,1))
 
-D = [1 1 1]
+# D = [1 1 1]
 
-x0 = [1, 2, 3]
+# x0 = [1, 2, 3]
+
+
+A = [0.0 1.0; -10.0 -1.0]
+B = [0.0, 1.0]
+Q = [300 0; 0 60]
+R = ones((1, 1))
+C = ones((2, 2))
+D = B
+x0 = [1, 2]
+
+#P = AREbyMatrixEquations(A, B, Q, R)
 
 @time simu(A, B, C, D, Q, R, x0)
