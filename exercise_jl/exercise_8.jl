d3bb86ac1db381d7acc6d54204171fe66de1d847
yoshimitsumatsutaIe <matsuta2718@gmail.com>
2021-08-17 21:04:08 +0900

    ie

--- exercise_jl/exercise_8.jl
+++ exercise_jl/exercise_8.jl
@@ -15,6 +15,13 @@ mutable struct InvertedPendulumParameter
 end
 
 
+f = @ode_def begin
+    dx = v
+    dv = -4*(-D*v + ω^2*l*m
+end M, m, L, l, D, d, g
+
+
+
 function dx(x, p, t)
 #function dx(x, t, M, m, L, l, D, d, g)
     """微分方程式"""
@@ -25,7 +32,7 @@ function dx(x, p, t)
     D = 0.01
     d = 0.01
     g = 9.80665
-
+    println("x = ", size(x))
     multi = [
         1 0 0 0
         0 0 1 0
@@ -51,53 +58,64 @@ function dx(x, p, t)
     Fa = inv(multi) * offset_x
     Fb = inv(multi) * offset_u
 
-    dx = Fa + Fb*u
+    
+    #dx = Fa + Fb.*u
+    dx = x
+    println(dx)
+    println("dx = ", size(dx), typeof(dx))
 end
 
 
-function ByLQR(M, m, L, l, D, d, g, x0=[0 0 2 0], )
+function ByLQR(M, m, L, l, D, d, g,)
 
-    # 原点近傍での線形化システム
-    multi = [
-        1 0 0 0
-        0 0 1 0
-        0 M + m 0 m * L
-        0 m * L 0 4/3 * m * l^2
-    ]
-    offset_x = [
-        0 1 0 0
-        0 0 0 1
-        0 -D 0 0
-        0 0 m * g * l -d
-    ]
-    offset_u = [
-        0
-        0
-        1
-        0
-    ]
+    # # 原点近傍での線形化システム
+    # multi = [
+    #     1 0 0 0
+    #     0 0 1 0
+    #     0 M + m 0 m * L
+    #     0 m * L 0 4/3 * m * l^2
+    # ]
+    # offset_x = [
+    #     0 1 0 0
+    #     0 0 0 1
+    #     0 -D 0 0
+    #     0 0 m * g * l -d
+    # ]
+    # offset_u = [
+    #     0
+    #     0
+    #     1
+    #     0
+    # ]
 
-    A = inv(multi) * offset_x
-    B = inv(multi) * offset_u
+    # A = inv(multi) * offset_x
+    # B = inv(multi) * offset_u
 
 
     
-    # リカッチ方程式を解く
-    Q = diagm(0 => [1,1,1,1]) .* 100  # 重み行列
-    R = ones((1,1))  # 重み行列
+    # # リカッチ方程式を解く
+    # Q = diagm(0 => [1,1,1,1]) .* 100  # 重み行列
+    # R = ones((1,1))  # 重み行列
 
-    S = zero(B)
-    P, _, _ = arec(A, B, R, Q, S)
+    # S = zero(B)
+    # P, _, _ = arec(A, B, R, Q, S)
 
-    K = inv(R) * B' * P  # 最適フィードバックゲイン
+    # K = inv(R) * B' * P  # 最適フィードバックゲイン
 
-    function input(x, K)
-        return u = -K * x
-    end
+    # function input(x, K)
+    #     return u = -K * x
+    # end
 
-    ### 数値シミュレーション ###
-    param = M, m, L, l, D, d, g
-    println(param)
+    # ### 数値シミュレーション ###
+    # param = M, m, L, l, D, d, g
+    # println(param)
+    x0 = [
+        0
+        0
+        2
+        0
+    ]
+    x0 = [0 0 2 0]
     tspan = (0.0, 1.0)  # 範囲を設定
     prob = ODEProblem(dx, x0, tspan)  # 問題を設定
     sol = solve(prob)  # ソルバで解く
@@ -114,9 +132,6 @@ l = L/2
 D = 1.0
 d = 1.0
 g = 9.8
-
-
-dx0 = dx([0 0 2 0], 0, 0)
 
 
 
