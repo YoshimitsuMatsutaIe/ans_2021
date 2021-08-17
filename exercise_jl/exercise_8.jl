c83a9e4930f0b4b37b4de99953fb81795ac2cef0
yoshimitsumatsutaIe <matsuta2718@gmail.com>
2021-08-18 08:50:25 +0900

    ie

--- exercise_jl/exercise_8.jl
+++ exercise_jl/exercise_8.jl
@@ -1,5 +1,6 @@
 using Plots
 using DifferentialEquations
+using ParameterizedFunctions
 using LinearAlgebra
 using MatrixEquations
 using CPUTime
@@ -17,35 +18,77 @@ end
 
 f = @ode_def begin
     dx = v
-    dv = -4*(-D*v + ω^2*l*m*sin(θ))/(-4*M + 3*m*cos(θ)^2 - 4*m) + 3*cos(θ)*(-d + dθ + g*l*m*sin(θ))/(l*(-4*M + 3*m*cos(θ)^2 -4*m)) -4/(-4*M + 3*m*cos(θ)^2 -4*m)
+    dv = -4*(-D*v + ω^2*l*m*sin(θ))/(-4*M + 3*m*cos(θ)^2 - 4*m) +
+    3*cos(θ)*(-d + ω + g*l*m*sin(θ))/(l*(-4*M + 3*m*cos(θ)^2 -4*m)) -
+    -4/(-4*M + 3*m*cos(θ)^2 -4*m) +
+    (K * [x; v; θ; ω])[1]
     dθ = ω
-    dω = 3*cos(θ)*(D*v + ω^2*l*m*sin(θ))/(l*())
-end M, m, L, l, D, d, g
+    dω = 3*cos(θ)*(D*v + ω^2*l*m*sin(θ))/(l*(-4*M + 3*m^2*cos(θ)^2 -4*m^2)) +
+    3*(M + m)*(-d + ω + g*l*m*sin(θ))/(l^2*(-4M + 3*m^2)*cos(θ)^2 - 4*m^2) + 
+    3*cos(θ)/(l*(-4M + 3*m*cos(θ)^2 - 4*m)) -
+    (K * [x; v; θ; ω])[1]
+end M m L l D d g K
 
 
 
-function dx(x, p, t)
-#function dx(x, t, M, m, L, l, D, d, g)
-    """微分方程式"""
-    M = 5.0
-    m = 1.0
-    L = 1.5
-    l = L/2
-    D = 0.01
-    d = 0.01
-    g = 9.80665
-    println("x = ", size(x))
+# function dx(x, p, t)
+# #function dx(x, t, M, m, L, l, D, d, g)
+#     """微分方程式"""
+#     M = 5.0
+#     m = 1.0
+#     L = 1.5
+#     l = L/2
+#     D = 0.01
+#     d = 0.01
+#     g = 9.80665
+#     println("x = ", size(x))
+#     multi = [
+#         1 0 0 0
+#         0 0 1 0
+#         0 M+m 0 m*l*cos(x[3])
+#         0 m*l*cos(x[3]) 0 4/3*m*l^2
+#     ]
+#     offset_x = [
+#         x[2]
+#         x[4]
+#         m*l*x[4]^2*sin(x[3])-D*x[2]
+#         m*g*l*sin(x[3])-d*x[4]
+#     ]
+#     offset_u = [
+#         0
+#         0
+#         1
+#         0
+#     ]
+
+#     #u = input(x, input_param)
+#     u = 0.0
+
+#     Fa = inv(multi) * offset_x
+#     Fb = inv(multi) * offset_u
+
+    
+#     #dx = Fa + Fb.*u
+#     dx = x
+#     println(dx)
+#     println("dx = ", size(dx), typeof(dx))
+# end
+
+
+function ByLQR(M, m, L, l, D, d, g,)
+
+    # 原点近傍での線形化システム
     multi = [
         1 0 0 0
         0 0 1 0
-        0 M+m 0 m*l*cos(x[3])
-        0 m*l*cos(x[3]) 0 4/3*m*l^2
+        0 M + m 0 m * L
+        0 m * L 0 4/3 * m * l^2
     ]
     offset_x = [
-        x[2]
-        x[4]
-        m*l*x[4]^2*sin(x[3])-D*x[2]
-        m*g*l*sin(x[3])-d*x[4]
+        0 1 0 0
+        0 0 0 1
+        0 -D 0 0
+        0 0 m * g * l -d
     ]
     offset_u = [
         0
@@ -54,72 +97,32 @@ function dx(x, p, t)
         0
     ]
 
-    #u = input(x, input_param)
-    u = 0.0
-
-    Fa = inv(multi) * offset_x
-    Fb = inv(multi) * offset_u
-
-    
-    #dx = Fa + Fb.*u
-    dx = x
-    println(dx)
-    println("dx = ", size(dx), typeof(dx))
-end
-
-
-function ByLQR(M, m, L, l, D, d, g,)
-
-    # # 原点近傍での線形化システム
-    # multi = [
-    #     1 0 0 0
-    #     0 0 1 0
-    #     0 M + m 0 m * L
-    #     0 m * L 0 4/3 * m * l^2
-    # ]
-    # offset_x = [
-    #     0 1 0 0
-    #     0 0 0 1
-    #     0 -D 0 0
-    #     0 0 m * g * l -d
-    # ]
-    # offset_u = [
-    #     0
-    #     0
-    #     1
-    #     0
-    # ]
-
-    # A = inv(multi) * offset_x
-    # B = inv(multi) * offset_u
+    A = inv(multi) * offset_x
+    B = inv(multi) * offset_u
 
 
     
-    # # リカッチ方程式を解く
-    # Q = diagm(0 => [1,1,1,1]) .* 100  # 重み行列
-    # R = ones((1,1))  # 重み行列
+    # リカッチ方程式を解く
+    Q = diagm(0 => [1,1,1,1]) .* 100  # 重み行列
+    R = ones((1,1))  # 重み行列
 
-    # S = zero(B)
-    # P, _, _ = arec(A, B, R, Q, S)
+    S = zero(B)
+    P, _, _ = arec(A, B, R, Q, S)
 
-    # K = inv(R) * B' * P  # 最適フィードバックゲイン
+    K = inv(R) * B' * P  # 最適フィードバックゲイン
+    println(K)
 
-    # function input(x, K)
-    #     return u = -K * x
-    # end
-
-    # ### 数値シミュレーション ###
-    # param = M, m, L, l, D, d, g
-    # println(param)
+    ### 数値シミュレーション ###
+    param = [M, m, L, l, D, d, g, K]
+    println(param)
     x0 = [
         0
         0
         2
         0
     ]
-    x0 = [0 0 2 0]
     tspan = (0.0, 1.0)  # 範囲を設定
-    prob = ODEProblem(dx, x0, tspan)  # 問題を設定
+    prob = ODEProblem(dx, x0, tspan, param)  # 問題を設定
     sol = solve(prob)  # ソルバで解く
 
     # plot
@@ -140,8 +143,11 @@ g = 9.8
 ByLQR(M, m, L, l, D, d, g)
 
 
+# p = [M, m, L, l, D, d, g]
+# x0 = [0; 0; 2; 0]
+# tspan = (0.0, 1.0)  # 範囲を設定
+# prob = ODEProblem(f, x0, tspan, p)  # 問題を設定
+# sol = solve(prob)  # ソルバで解く
 
 
-
-
-
+# plot(sol)
