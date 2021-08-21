91ab04a68b4362675fd05efac4eb2bfe4de312dd
yoshimitsumatsutaIe <sbb03104@edu.osakafu-u.ac.jp>
2021-08-21 17:33:34 +0900

    gakk

--- exercise_jl/exercise_4.jl
+++ exercise_jl/exercise_4.jl
@@ -1,6 +1,5 @@
 using CPUTime
 using DifferentialEquations
-using ParameterizedFunctions
 using Plots
 
 f = @ode_def begin
@@ -10,7 +9,9 @@ f = @ode_def begin
 end m c k xg Kp Ki Kd
 
 
+
 function do_1()
+    """1つめのやつ"""
     m = 1.0
     c = 1.0
     k = 1.0
@@ -39,5 +40,46 @@ function do_1()
 end
 
 
+function do_2()
+    """2つめのやつ"""
+    m = 1.0
+    c = 1.0
+    k = 1.0
+    xg = 1.0
 
-@time do_1()
+
+    n = 50  # アニメの枚数
+    K_min, K_max = 0.0, 3.0
+    Kp = [i for i in K_min:(K_max-K_min)/n:K_max]
+    Ki = 5.0
+    Kd = 0.05
+
+    sols = []
+    tspan = (0.0, 10.0)  # 解く範囲を設定
+
+    for j in 1:n
+        x0 = [0.0, 0.0, Kp[j]*(xg - 1.0)]  # 初期値を設定
+        p = [m, c, k, xg, Kp[j], Ki, Kd]
+        prob = ODEProblem(f ,x0 ,tspan, p)
+        sol = solve(prob)  # ソルバで解く
+        push!(sols, sol)
+    end
+
+    anim = @animate for i in 1:1:length(sols)
+        now = round(Kp[i], digits=1)
+        s = "Kp = " * string(now) * ", Ki = " * string(Ki) * ", Kd = " * string(Kd)
+        plot(
+            sols[i],
+            ylims = (-5, 5), annotate = (5, -1, s),
+        )
+    end
+    mp4(anim, "example_4_jl.mp4", fps = 60)
+
+
+
+end
+
+
+#@time do_1()
+
+@time do_2()
