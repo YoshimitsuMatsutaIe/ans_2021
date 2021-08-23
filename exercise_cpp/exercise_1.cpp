081f0b13ff4d4c5824ba540b8dfd7e62e2baf043
yoshimitsumatsutaIe <matsuta2718@gmail.com>
2021-08-24 01:41:03 +0900

    ie

--- exercise_cpp/exercise_1.cpp
+++ exercise_cpp/exercise_1.cpp
@@ -1,16 +1,17 @@
 # include <iostream>
 # include <fstream>
-#include <cmath>
-//using namespace std;
 
 
 /**
- * @brief 素数判定関数
+ * @brief 素数判定関数．素直な実装．
  * @param[in]   n  調べたい正の整数
 */
 bool isPrime(int n){
     if (n < 2){
         return false;
+    }
+    else if(n == 2){
+        return true;
     }
     else {
         for (int i = 2; i < n; i++){
@@ -20,22 +21,36 @@ bool isPrime(int n){
             else {
                 continue;
             }
-            return true;
         }
     }
+    return true;
+}
+
+
+/**
+ * @brief 2.を実行
+ * @param[in]   n0  はじめ
+ * @param[in]   n1  おわり
+*/
+void do_2(int n0, int n1){
+    int count = 0;
+    for (int i = n0; i < n1 + 1; i++){
+        if (isPrime(i)){
+            count += 1;
+        }
+    }
+
+    std::cout << n0 << " から " << n1 << " に素数は " << std::endl;
+    std::cout << count << std::endl;
+    std::cout << "個ある" << std::endl;
 }
 
 int main(){
-    int n;
-    std::cout << "type number " << std::endl;
-    std::cin >> n;
-    std::cout << n << " is..." << std::endl;
 
-    if (isPrime(n)){
-        std::cout << "prime number!!" << std::endl;
-    }
-    else {
-        std::cout << "orz" << std::endl;
-    }
+    int n0, n1;
+    n0 = 1;
+    n1 = 999961;
+    do_2(n0, n1);
+
     return 0;
 }
