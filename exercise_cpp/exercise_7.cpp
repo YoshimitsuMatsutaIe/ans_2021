ea74ea5da8852002e3e8bb1d6138285f9d266c93
yoshimitsumatsutaIe <matsuta2718@gmail.com>
2021-08-25 00:40:08 +0900

    ie

--- exercise_cpp/exercise_7.cpp
+++ exercise_cpp/exercise_7.cpp
@@ -1,10 +1,43 @@
+/**
+ * @file 
+ * @brief 逆運動学を解くシミュレーション．同じディレクトリにEigenディレクトリをおいておく．
+ * @author matsuta
+ */
+
+
 # include <iostream>
 # include <fstream>
 # include <cmath>
-# include "Eigen/Core"  //ヘッダにおいておく
+# include <Eigen/Core>  // Eigen
 
-const static int N = 3;  // システムの次元
+const static int N = 3;  // タスク空間の次元
+const static int M = 7;  // 配置空間の次元
 
-Eigen::MatrixXf A=Eigen::MatrixXf::Zero(2,2);
-    A(0,0)=2;
-    A(1,1)=5;
+
+
+class BaxterKinematics{
+    public:
+        double L;  // アーム長さ
+        double l;
+        double L0;
+        double L1;
+        double L2;
+        double L3;
+        double L4;
+        double L5;
+        double L5;
+
+        void joint_origin_Wo(Eigen::Vector q){
+
+        }
+};
+
+
+
+int main(){
+
+    Eigen::VectorXd q0 = Eigen::VectorXd::Ones(2, 7);
+
+
+    return 0;
+}
