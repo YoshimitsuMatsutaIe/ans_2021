fa43ce6409aa8293ba614a0706c529ce7b21494b
yoshimitsumatsutaIe <matsuta2718@gmail.com>
2021-08-27 03:43:13 +0900

    cpp

---
+++ exercise_cpp/exercise_8.cpp
@@ -0,0 +1,40 @@
+/**
+ * @file 
+ * @brief 倒立振子制御の数値シミュレーション
+ * @author matsuta
+ * @note Eigenが必要（今の所ビルドできない．原因不明）
+ */
+
+
+#include <iostream>
+#include <fstream>
+#include <math.h>
+#include "Eigen/Core"  // Eigen
+// #include "Eigen/Dense"
+// #include "Eigen/QR"
+//#include "Eigen/Eigen"
+
+
+using std::cout;
+using std::endl;
+
+const static double PI = 3.141592653589793;  // 円周率
+
+
+
+
+
+Eigen::MatrixXd Runge_solver(){
+
+    
+}
+
+
+
+
+int main(){
+
+
+
+    return 0;
+}
