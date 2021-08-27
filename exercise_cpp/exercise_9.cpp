af8bf79052f0b0802c457ab21a469ad1e57b2830
yoshimitsumatsutaIe <sbb03104@edu.osakafu-u.ac.jp>
2021-08-27 21:13:13 +0900

    gakko

--- exercise_cpp/exercise_9.cpp
+++ exercise_cpp/exercise_9.cpp
@@ -16,6 +16,7 @@ using std::cout;
 using std::endl;
 using std::vector;
 using std::map;
+using std::pair;
 
 /**
     @brief  Node
@@ -80,13 +81,44 @@ vector<Option> calc_options(int x, int y, int x_max, int y_max, vector<vector<bo
     return options;
 }
 
+int find_MinCostNode_id(map<int, Node> m){
+    int min_cost_id;
+    auto begin = m.begin();
+    auto end = m.end();
+
+    map<int, Node>::iterator pm;
+    //int min_cost_id = m[begin->first];
+    while (pm != m.end()){
+        if (pm == m.begin()){
+            min_cost_id = pm->first;
+        }
+        else if (pm->second.cost < m[min_cost_id].cost){
+            min_cost_id = pm->first;
+        }
+    }
+    return min_cost_id;
+}
+
 
 std::tuple<vector<int>, vector<int>, double, map<int, Node>> planning(int start_x, int start_y, int goal_x, int goal_y){
     Node start_node = {start_x, start_y, 0.0, -1};
-    Node goal_Node = {goal_x, goal_y, -1, -2};
+    Node goal_node = {goal_x, goal_y, -1, -2};
+    Node temp_node;
+    int temp_x, temp_y, temp_id;
 
-    map<int, Node> open_Set;
-    map<int, Node> closed_set;
+    map<int, Node> open_set;  // 未決定のノード
+    map<int, Node> closed_set;  // 決定済みのノード
+
+    int start_id = 0;
+    open_set.insert(pair<int, Node>(start_id, start_node));
+
+    while (temp_x == goal_node.x && temp_y == goal_node.y){
+        temp_id = find_MinCostNode_id(open_set);
+        temp_node = open_set[temp_id];
+
+        if 
+    }
+
 
 }
 
