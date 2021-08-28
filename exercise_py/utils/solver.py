6bac6babece852e6a6f9f5bcc9a824b77470cd26
yoshimitsumatsutaIe <matsuta2718@gmail.com>
2021-08-28 23:08:01 +0900

    ie

---
+++ exercise_py/utils/solver.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+
+def hello():
+    print("hello")
+    return
+
+
+
+class sol:
+    def __init__(self, y):
+        self.y = y
+
+def jisaku_solve_RungeKuttar(dx, x0, tspan, dt):
+    """ルンゲクッタソルバ"""
+    t = range(tspan)
+    y
+
+if __name__ == "__main__":
+    pass
